d829e10fdd6e9ab0fdebd65d15c8d91770853e23
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-14 15:49:11 +0000

    added function to place ship randomily on board

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from random import randint
 
+# Initialize scores for computer and player
 scores = {"computer": 0, "player": 0}
 
 class Board:
@@ -9,10 +10,28 @@ class Board:
     """
 
     def __init__(self, size, num_ships, name, type):
+
+        """
+        Initialize the board with specified size, number of ships, player's name, and board type.
+
+        Parameters:
+            size (int): Size of the board.
+            num_ships (int): Number of ships to be placed on the board.
+            name (str): Name of the player.
+            type (str): Type of the board (player or computer).
+        """
         self.size = size
         self.num_ships = num_ships
         self.type = type
         self.name = name
-        self.board = [['O' for _ in range(size)] for _ in range(size)]
+        self.board = [['.' for x in range(size)] for y in range(size)]
         self.guesses = []
         self.ships = []
+
+        def place_ships(self):
+            """
+            Randomily place ships on board. col is colum, 
+            """
+            for _ in range(self.num_ships):
+                ship_row = randint(0, self.size -1)
+                ship_col = randint(0, self.size -1)
